a305f402b7968e745216653b74bd9726bba4217f
Soumya <jenasoumya2004@gmail.com>
2024-01-15 19:03:10 +0530

    added seekbar,used js to add song playing functionslity

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 console.log('Js is working');
+let currentSong = new Audio();
+
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
 
 async function getsongs() {
     let a = await fetch('http://127.0.0.1:5500/songs/');
@@ -17,26 +32,64 @@ async function getsongs() {
     return songs;
 }
 
+const playMusic = (track, pause = false) => {
+    currentSong.src = "/songs/" + track;
+    if (!pause) {
+        currentSong.play();
+        play.src = "pause.svg";
+    }
+    document.querySelector(".songinfo").innerHTML = track.replaceAll("%20", " ");
+    document.querySelector(".songtime").innerHTML = "00:00/00:00";
+}
+
 async function main() {
+
+
     let songs = await getsongs();
-    console.log(songs);
+    //console.log(songs);
+    playMusic(songs[0], true);
 
     let songUl = document.querySelector(".songlist").getElementsByTagName("ul")[0];
     for (const song of songs) {
         songUl.innerHTML = songUl.innerHTML + `<li>
                 <img class="invert" src="music.svg" alt="music">
                 <div class="info">
-                    <div style="font-size: 7px;">${song.replaceAll("%20", " ") }</div>
+                    <div style="font-size: 7px;">${song.replaceAll("%20", " ")}</div>
                     <div style="font-size: 10px;">SRJ</div>
                 </div>
                 <div class="playnow">
                     <span>Play Now</span>
                     <img class="invert" src="play.svg" alt="play">
-                </div>
-            </li>`;
+                </div></li>`;
     }
-    // var audio = new Audio(songs[0]);
-    // audio.play();
+
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach((e) => {
+        e.addEventListener("click", () => {
+
+            console.log(e.querySelector(".info").firstElementChild.innerHTML);
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+        })
+    })
+
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play();
+            play.src = "pause.svg";
+        } else {
+            currentSong.pause();
+            play.src = "play.svg";
+        }
+    })
+
+    currentSong.addEventListener("timeupdate", () => {
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`;
+        document.querySelector(".circle").style.left = `${(currentSong.currentTime / currentSong.duration) * 100}%`;
+    });
+
+    document.querySelector(".seekbar").addEventListener("click", (e) => {
+        document.querySelector(".circle").style.left = `${(e.offsetX / e.target.getBoundingClientRect().width) * 100}%`;
+        currentSong.currentTime = (e.offsetX / e.target.getBoundingClientRect().width) * currentSong.duration;
+    })
 }
 
 main();
